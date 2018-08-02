db761fe3e6859ae0fca959aaf6d0864ba526ea2a
Lucien Greathouse <me@lpghatguy.com>
2018-08-02 11:17:48 -0700

    Fix quotes on default font size snippet

--- other/UserPackages/default_font_size.py
+++ other/UserPackages/default_font_size.py
@@ -7,6 +7,6 @@ class ResetFontSizeToUserDefaultsCommand(sublime_plugin.ApplicationCommand):
     def run(self):
         s = sublime.load_settings("Preferences.sublime-settings")
 
-        if s.has('default_font_size'):
-            s.set('font_size', s.get('default_font_size'))
+        if s.has("default_font_size"):
+            s.set("font_size", s.get("default_font_size"))
             sublime.save_settings("Preferences.sublime-settings")
